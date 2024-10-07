6dc62a7608fae1f1f08ef1a01f42677b36b326e3
Noémie Prévost <noemie.a.pr@gmail.com>
2024-10-07 15:55:16 -0400

    Ajout mouvements
    Ophelia se dirige lentement vers les objets au lieu de se téléporter

--- Assets/ClickManagerScript.cs
+++ Assets/ClickManagerScript.cs
@@ -6,13 +6,43 @@ using UnityEngine;
 public class ClickManagerScript : MonoBehaviour
 {
 
+    float moveSpeed = 3;
+    float moveAccuracy = 0.15f;
+
+
     public Transform player;
     public void GoToItem(ItemData item)
     {
-        player.position = item.goToPoint.position;
+
+        StartCoroutine(MoveToPoint(item.goToPoint.position));
        
 
     }
 
+    public IEnumerator MoveToPoint(Vector2 point)
+    {
+
+        // set direction
+        Vector2 positionDifference = point - (Vector2)player.position;
+
+        // stop when we are near the point
+        while (positionDifference.magnitude > moveAccuracy)
+        {
+
+
+            // move in direction frame by frame
+            player.Translate(moveSpeed*positionDifference.normalized*Time.deltaTime);
+            yield return null;
+
+            // set direction
+            positionDifference = point - (Vector2)player.position;
+
+        }
+
+        player.position = point;
+
+        yield return null;
+
+    }
 
 }
