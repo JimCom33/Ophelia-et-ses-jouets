88941867760139252a63363c877a9fd0638ee49a
Jimmy Comeault <jimmy.comeault@gmail.com>
2024-12-03 18:38:53 -0500

    Ajout menu, texte, fleche

---
+++ Assets/Scripts/MoveToLibrary.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MoveToLibrary : MonoBehaviour
+{
+    public Texture2D arrow;
+    private Texture2D defaultCursor;
+    public Vector2 hotspot = Vector2.zero;
+
+    private bool isHovering = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.SetCursor(defaultCursor, hotspot, CursorMode.Auto);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("entre");
+        if (other.CompareTag("Mouse"))
+        {
+            
+            isHovering = true;
+            Cursor.SetCursor(arrow, hotspot, CursorMode.Auto);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        Debug.Log("sortie");
+        if (other.CompareTag("Mouse"))
+        {
+            
+            isHovering = false;
+            Cursor.SetCursor(defaultCursor, hotspot, CursorMode.Auto);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isHovering && Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene("OpheliaLibrary");
+        }
+    }
+}
---
+++ Assets/Scripts/Text.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Text : MonoBehaviour
+{
+    public Image fadeBackground;
+    public TMP_Text message;
+    private float messageTimer;
+
+    internal void ShowText(string text)
+    {
+        ShowText(text, 5f);
+    }
+
+    internal void ShowText(string text, float duration)
+    {
+        message.text = text;
+        fadeBackground.enabled = true;
+        messageTimer = duration;
+    }
+
+    private void Update()
+    {
+        if (messageTimer > 0)
+        {
+            messageTimer -= Time.deltaTime;
+            if (messageTimer <= 0)
+            {
+                CLear();
+            }
+        }
+    }
+
+    private void CLear()
+    {
+        fadeBackground.enabled = false;
+        message.text = "";
+    }
+
+    private void Start()
+    {
+        CLear();
+    }
+}
